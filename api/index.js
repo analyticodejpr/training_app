00dde5ece2858949a3e595dec8fc2f66115e1707
analyticodejpr <analyticodejpr@users.noreply.github.com>
2026-04-03 20:05:34 +0200

    Expose startup error as JSON for debugging

--- api/index.js
+++ api/index.js
@@ -1,2 +1,12 @@
-const app = require('./app');
+let app;
+try {
+  app = require('./app');
+} catch (err) {
+  // If Express fails to load, return the error as JSON so we can debug
+  app = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 500;
+    res.end(JSON.stringify({ startup_error: err.message, stack: err.stack }));
+  };
+}
 module.exports = app;
